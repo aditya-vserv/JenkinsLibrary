caa4375a88cc435f6c79e7f5721b450506e566b8
Aditya Sharma <aditya.s@vserv.com>
2023-02-23 18:13:54 +0530

    Initial Commit

---
+++ JenkinsFramework.xcframework/ios-arm64/JenkinsFramework.framework/Headers/JenkinsFramework.h
@@ -0,0 +1,18 @@
+//
+//  JenkinsFramework.h
+//  JenkinsFramework
+//
+//  Created by Admin_Vserv on 20/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JenkinsFramework.
+FOUNDATION_EXPORT double JenkinsFrameworkVersionNumber;
+
+//! Project version string for JenkinsFramework.
+FOUNDATION_EXPORT const unsigned char JenkinsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JenkinsFramework/PublicHeader.h>
+
+
